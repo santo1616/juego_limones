97436630a06063e244ab1ff4746abfdb3899754f
santo1616 <santo1616@gmail.com>
2026-04-16 21:26:42 -0500

    mover izquierda y mover derecha

--- limones.js
+++ limones.js
@@ -5,8 +5,7 @@ const ALTURA_SUELO=20;
 const ALTURA_PERSONAJE=60;
 const ANCHO_PERSONAJE=40;
 
-
-
+let personajeX=canvas.width/2;
 
 function iniciar(){
     dibujarSuelo();
@@ -22,6 +21,28 @@ function dibujarSuelo(){
 function dibujarPersonaje(){
 
     ctx.fillStyle="yellow";
-    ctx.fillRect(canvas.width/2,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+    ctx.fillRect(personajeX,canvas.height-(ALTURA_SUELO+ALTURA_PERSONAJE),ANCHO_PERSONAJE,ALTURA_PERSONAJE);
+
+}
+function moverIzquierda(){
+    personajeX=personajeX-10;
+    actualizarPantalla();
+    
+}
+function moverDerecha(){
+    personajeX=personajeX+10;
+    actualizarPantalla();
+    
+}
+
+function actualizarPantalla(){
+    limpiarCanva();
+    dibujarSuelo();
+    dibujarPersonaje();
+
+}
+
+function limpiarCanva(){
+    ctx.clearRect(0,0,canvas.width,canvas.height);
 
 }
